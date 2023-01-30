7c57cac3244d223874895704ae277b4e48f6ca2b
bhuwanjungthapa <bhuwanjung115@gmail.com>
2023-01-30 18:57:22 +0545

    user auth

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const fileUpload = require('express-fileupload');
+
+const dotenv = require('dotenv')
+dotenv.config()
+
+const PORT = process.env.PORT || 3000;
+const dbConn = require('./app/config/db');
+dbConn();
+const cors = require('cors');
+
+app.use(express.json());
+app.use(fileUpload());
+app.use(cors());
+
+const userRoutes = require('./app/routes/route.user');
+const bannerRoutes = require('./app/routes/route.banner');
+const authRoutes = require('./app/routes/route.auth');
+
+app.use('/api/v1', authRoutes);
+app.use('/api/v1/user', userRoutes);
+app.use('/api/v1/banner', bannerRoutes);
+
+const verifyJWT = require('./app/middlewares/verifyJWT');
+// app.use(verifyJWT);
+
+app.get('/api/v1/test', (req, res) => {
+    res.send('Hello World!' + req.user);
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
